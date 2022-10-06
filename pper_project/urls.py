b1d8f6b447150676f7001ddabbdec77eb97bc2c0
“rhm539” <rhm539@gmail.com>
2022-10-06 10:05:08 +0000

    777

--- pper_project/urls.py
+++ pper_project/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True,
+         template_name='commons/login.html'), name='login'),
+    #path('login/', auth_views.LoginView.as_view(template_name='commons/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
