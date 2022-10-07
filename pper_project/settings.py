567d1337763c8415774ed52804a468740184e40d
“rhm539” <rhm539@gmail.com>
2022-10-07 10:42:45 +0000

    07/10/2022

--- pper_project/settings.py
+++ pper_project/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
     'dashboard.apps.DashboardConfig',
+    'setup.apps.SetupConfig',
+    # install
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -99,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -115,15 +122,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR/'static/')
 
-# Media Files
-MEDIA_ROOT = os.path.join(BASE_DIR/'media/')
+MEDIA_ROOT = (BASE_DIR/"media/")
+
 MEDIA_URL = '/media/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'web'
